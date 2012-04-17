da40c1d79f8eb37b326ef05c42dc405ad52ccc82
Taku Yamada <t-yamada@groovenauts.jp>
2012-04-17 16:14:02 +0900

    rails new actioncontroller

---
+++ actioncontroller/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Actioncontroller::Application.config.secret_token = '32be80a02a4488566e5a1504911a2f62913f6464b4f2193f9d8ecfce1b3772ba9ba8317b3f7781928873907de4a5822403ffd004271ea1da9e3b8b52ade42670'
